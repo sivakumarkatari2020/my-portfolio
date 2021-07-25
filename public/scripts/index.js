b013d92d4a96111f5223f37576e86bd3388be000
siva kumar <sivakumarkatari2556@gmail.com>
2021-07-25 15:16:25 +0530

    all sections added

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,37 +1,14 @@
 //drop downs
-let acc1 = document.getElementById("down1");
-let acc2 = document.getElementById("down2");
-let acc3 = document.getElementById("down3");
-let acc4 = document.getElementById("down4");
-let acc5 = document.getElementById("down5");
-let acc6 = document.getElementById("down6");
 
-let accordions = new Array(acc1,acc2,acc3,acc4,acc5,acc6);
+let Accordions = document.getElementsByClassName('acc');
 
-function openAcc(e){
-    let id = e.target.attributes.id.nodeValue;
-    
-    let acc = document.getElementById(id); 
-    let para = document.getElementsByClassName(id);
-
-    if(para[0].style.display == 'none'){
-        para[0].style.display = 'block';
-        acc.style.transform = "scale(0.7) rotate(-90deg)";
-    }
-    else{
-        para[0].style.display = 'none';
-        acc.style.transform = "scale(0.7) rotate(90deg)";
-    }
+for(let i=0;i<Accordions.length;i++){
+    Accordions[i].addEventListener('click',function(){
+        let active_ele = document.getElementsByClassName('active');
+        for(let j=0;j<active_ele.length;j++){
+            active_ele[j].classList.toggle('active');
+        }
+        this.classList.toggle('active');
+    })
 }
 
-function closeAcc(ele){
-    console.log(ele);
-    let acc = document.getElementById(ele);
-    acc.style.transform = 'rotate(45deg) scale(0.7)';
-
-    acc.toggleAttribute('onClick');
-    acc.setAttribute('onClick','openAcc(e)');
-
-    let para = document.getElementsByClassName(ele);
-    para[0].style.display = 'none';
-}
